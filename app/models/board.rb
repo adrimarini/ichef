fc2d843cb4e45905750eac6857c6d9ede97b382c
Adrianna Marini <adrimarini>
2016-07-27 09:50:46 -0700

    added ichef logo in nav and made it so unlinked images appear on cookbook page relevant to board

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,13 @@ class Board < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :recipe_steps, dependent: :destroy
-  has_many :images, dependent: :destroy 
+  has_many :images, dependent: :destroy
+
+  def main_image
+    if self.images.count > 0
+      self.images.first.url
+    else
+      "http://placekitten.com/350/400"
+    end
+  end
 end
